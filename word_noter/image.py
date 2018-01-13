f170e204fb27ef406ecaa104bd7d8844ec1ac95c
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-01-13 10:23:35 +0800

    add hough line test

--- word_noter/image.py
+++ word_noter/image.py
@@ -1,3 +1,5 @@
+import cv2
+import numpy as np
 from PIL import Image
 
 
@@ -10,12 +12,45 @@ def convert_frame_to_image(frame):
     return Image.fromarray(frame)
 
 
-if __name__ == '__main__':
-    import time
-    from .camera import ImageCapture
+def get_hough_lines(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    return cv2.HoughLines(edges, 1, np.pi / 180, 200)
 
-    cap = ImageCapture()
-    with cap:
-        time.sleep(5)
-        frame = cap.capture()
-    image = convert_frame_to_image(frame)
+
+def get_median_hough_line(image):
+    """Get a hough line that with median theta of all hough lines of the image
+
+    :param image:
+    :return:
+    """
+    lines = get_hough_lines(image)
+    lines = lines.reshape((-1, 2))
+    median_index = np.argsort(lines, axis=0)[len(lines) // 2][1]
+    return lines[median_index]
+
+
+if __name__ == '__main__':
+    # import time
+    # from .camera import ImageCapture
+    #
+    # cap = ImageCapture()
+    # with cap:
+    #     time.sleep(5)
+    #     frame = cap.capture()
+    # image = convert_frame_to_image(frame)
+    img = cv2.imread('test.png')
+    line = get_median_hough_line(img)
+    rho, theta = line
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
+
+    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    cv2.imwrite('test_out.jpg', img)
