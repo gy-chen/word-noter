f6680b91f4fb1d8a810ce0990704fc854421d52d
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-02-21 21:06:29 +0800

    update

--- word_noter/task.py
+++ word_noter/task.py
@@ -1,6 +1,5 @@
 import asyncio
 from .websocket import create_word_sender_server
-from .image.task import create_main_task, recognized_words
 
 
 def create_recognize_from_image_task():
@@ -8,13 +7,29 @@ def create_recognize_from_image_task():
 
     :return:
     """
+    from .image.task import create_main_task, recognized_words
     recognize_words_task = create_main_task(recognized_words)
     server = create_word_sender_server(recognized_words)
     return asyncio.gather(recognize_words_task, server)
 
 
+def create_fake_words_task():
+    """Generate a task that keep generating fake words and the words through WebSocket.
+
+    This task is useful for testing.
+
+    :return:
+    """
+    from .fake import generate_words, fake_words
+    generate_words_task = generate_words(fake_words)
+    server = create_word_sender_server(fake_words)
+    return asyncio.gather(generate_words_task, server)
+
+
 if __name__ == '__main__':
     import logging
+
+    # TODO maybe put this to console script
     logging.basicConfig(level=logging.DEBUG)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(create_recognize_from_image_task())
+    loop.run_until_complete(create_fake_words_task())
